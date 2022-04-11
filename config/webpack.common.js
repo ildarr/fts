8292a89cb2bd9def3da201407263e6c68385382e
Ildar Sakaev <isakaev@bellintegrator.com>
2022-04-11 10:33:26 +0500

    add test webpack config

---
+++ config/webpack.common.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//const { src, build, public } = require('./paths');
+const { src, build, assets } = require('./paths');
+
+module.exports = {
+  entry: [`${src}/index.tsx`],
+
+  output: {
+    path: build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+
+    // Очищает директорию dist перед обновлением бандла
+    // Свойство стало доступно с версии 5.20.0, до этого использовался CleanWebpackPlugin
+    clean: true,
+  },
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: assets, to: 'assets' }],
+    }),
+
+    new HtmlWebpackPlugin({
+      favicon: `${src}/images/favicon.ico`,
+      template: `${src}/index.html`,
+    }),
+
+    new webpack.WatchIgnorePlugin({
+      paths: [/css\.d\.ts$/],
+    }),
+  ],
+
+  module: {
+    rules: [
+     // { test: /\.(js|jsx)$/, use: ['babel-loader'] },
+
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  resolve: {
+    modules: [src, 'node_modules'],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+    alias: {
+      '@': src,
+     'assets': assets,
+    },
+  },
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,47 @@
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+
+  //eslint-loader ??
+  // eslint-webpack-plugin
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          '@teamsupercell/typings-for-css-modules-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: {
+                localIdentName: '[local]__[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
